b242b80ee2b1f4bf00ccbbcf4153179874af9c88
devejs <ssej0221@gmail.com>
2020-11-26 01:39:25 +0900

    #4 fix AttributeError

--- chatApp/views.py
+++ chatApp/views.py
@@ -4,13 +4,12 @@ from django.http import HttpResponse
 from django.conf import settings
 import os
 
-
 class ReactAppView(View):
     def get(self, request):
         try:
-            with open(os.path.join(str(settings.ROOT_DIR), 'frontend', 'build', 'index.html')) as file:
+            with open(os.path.join(str(settings.BASE_DIR), 'frontend', 'build', 'index.html')) as file:
                 return HttpResponse(file.read())
-        except:
+        except Exception as e:
             return HttpResponse(
                 """
             index.html not found ! build your React app !!
